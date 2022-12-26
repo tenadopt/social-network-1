aa6b1bdb4db11e4cf015df6893e73ad4084113d5
Tenad <46803834+tenadopt@users.noreply.github.com>
2022-12-26 06:52:34 +0300

    creating start page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App-wrapper">
+            <header className='header'>
+                <div>
+                    Hello world
+                    <img src='https://www.vectorstock.com/royalty-free-vector/basketball-logo-vector-2216393'/>
+                </div>
+            </header>
+            <nav className='nav'>
+                <div>
+                    Message
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                    Main Content
+                </div>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
