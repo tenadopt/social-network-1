477fe12d66a1c08154da06157fc36eb3c30fc33d
Tenad <46803834+tenadopt@users.noreply.github.com>
2023-01-04 15:59:50 +0300

    add music and settings content

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 export const Settings = () => {
     return (
         <div>
-
+            <li>
+                <span>Volume</span>
+            </li>
         </div>
     );
 };
