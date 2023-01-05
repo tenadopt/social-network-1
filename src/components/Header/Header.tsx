71332a0cd780481a700a012fc2c5118ecdeec6c6
Tenad <46803834+tenadopt@users.noreply.github.com>
2023-01-05 16:22:50 +0300

    add structure header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,6 +4,8 @@ import classes from '../Header/Header.module.css';
 export const Header = () => {
     return (
             <header className={classes.header}>
+                Logo
+                Menu
                 <img
                     src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiRppt6rof60oGOUxQNjJOKCF1cjabgf2I_muZYMn4oMATqubTUcW4qjfWtHV5S6nsl9w&usqp=CAU'/>
             </header>
