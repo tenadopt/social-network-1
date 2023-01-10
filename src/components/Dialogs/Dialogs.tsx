9453d98f1624d6b41eaf85e7f1e529b17087182a
Tenad <46803834+tenadopt@users.noreply.github.com>
2023-01-10 13:59:10 +0300

    add messages

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 export const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialog}>
-                    Yury
+                <div className={s.dialog} >
+                    <NavLink to='/dialogs/1' >Yury</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Artem
+                    <NavLink to="/dialogs/2">Artem</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Happy
+                    <NavLink to="/3">Happy</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Yug
+                    <NavLink to="/dialogs/4">Yug</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Antony
+                    <NavLink to="/dialogs/5">Antony</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Lev
+                    <NavLink to="/dialogs/6">Lev</NavLink>
                 </div>
 
             </div>
