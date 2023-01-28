3caa1dcf41ac43d74bf09145c845c661e9a01656
Tenad <46803834+tenadopt@users.noreply.github.com>
2023-01-28 23:02:41 +0300

    refactoring Profileinfo

--- src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.tsx
@@ -4,11 +4,15 @@ import s from './ProfileInfo.module.css'
 export const ProfileInfo = () => {
     return (
         <div>
-            <div> <img
+            <div>
+                <img
                     src='https://www.metoffice.gov.uk/binaries/content/gallery/metofficegovuk/hero-images/advice/maps-satellite-images/satellite-image-of-globe.jpg'/>
+
             </div>
             <div className={s.descriptionBlock}>
+
                 ava+sescription
+
             </div>
         </div>
     );
