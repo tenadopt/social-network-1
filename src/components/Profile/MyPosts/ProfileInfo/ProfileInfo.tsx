aa5e702589b47ed985067f3aad90b0a700c98b03
Tenad <46803834+tenadopt@users.noreply.github.com>
2023-02-10 23:00:07 +0300

    refactoring

--- src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.tsx
@@ -6,9 +6,7 @@ export const ProfileInfo = () => {
         <div>
             <div>
                 <img
-
                     src='https://www.metoffice.gov.uk/binaries/content/gallery/metofficegovuk/hero-images/advice/maps-satellite-images/satellite-image-of-globe.jpg'/>
-
             </div>
             <div className={s.descriptionBlock}>
                 ava+sescription
